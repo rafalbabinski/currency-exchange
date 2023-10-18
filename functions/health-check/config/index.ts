901a27c5a0388b2eeddaadea960aa75d45cd243d
Rafał Bąbiński <rafal.babinski@tsh.io>
2023-10-18 09:28:05 +0200

    feat: add api key config

--- functions/health-check/config/index.ts
+++ functions/health-check/config/index.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 
 const loadEnvs = (env: any) => ({
   appName: env.APP_NAME,
+  apiKey: env.API_KEY,
 });
 
 const validateConfig = (config: any) => {
   const schema = z.object({
     appName: z.string().min(1),
+    apiKey: z.string().min(1),
   });
 
   try {
