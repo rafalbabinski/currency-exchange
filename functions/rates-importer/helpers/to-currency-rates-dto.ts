bb53230bbcffd0aefa4123a4db36422810157e3c
Rafał Bąbiński <rafal.babinski@tsh.io>
2023-11-06 10:14:27 +0100

    feat: add rates importer lambda

--- functions/rates-importer/helpers/to-currency-rates-dto.ts
+++ functions/rates-importer/helpers/to-currency-rates-dto.ts
@@ -13,14 +13,14 @@ export const toCurrencyRatesDto = (response: RatesResponse): CurrencyRatesDto =>
   const currencyName = response.currency;
   const createdAt = new Date().toISOString();
 
-  const rates: Rates = {}
-    ;
+  const rates: Rates = {};
+
   response.rates
     .filter((rate) => rate.currency !== currencyName)
     .forEach((rate) => {
       rates[rate.currency] = rate.rate;
     });
-  
+
   return {
     currencyName,
     createdAt,
